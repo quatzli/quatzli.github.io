ac4f3582465f73e535a03808d635aa0eed8086b6
quatzli <holger.hanneken@gmail.com>
2020-10-30 14:42:38 +0100

    small changes

--- corona/script.js
+++ corona/script.js
@@ -1,50 +1,55 @@
-var csvData = [];
+var csvData = []
 
-// const population = {
-//     "Samtgemeinde Dörpen": 15672,
-//     "Einheitsgemeinde Emsbüren": 9886,
-//     "Samtgemeinde Freren": 5113,
-//     "Einheitsgemeinde Geeste": 11337,
-//     "Stadt Haren (Ems)": 23029,
-//     "Stadt Haselünne": 12840,
-//     "Samtgemeinde Herzlake": 4097,
-//     "Samtgemeinde Lathen": 11122,
-//     "Samtgemeinde Lengerich": 22234,
-//     "Stadt Lingen (Ems)": 54708,
-//     "Stadt Meppen": 34862,
-//     "Samtgemeinde Nordhümmling": 12226,
-//     "Stadt Papenburg": 35268,
-//     "Einheitsgemeinde Rhede (Ems)": 4218,
-//     "Einheitsgemeinde Salzbergen": 7590,
-//     "Samtgemeinde Sögel": 15923,
-//     "Samtgemeinde Spelle": 8442,
-//     "Einheitsgemeinde Twist": 9621,
-//     "Samtgemeinde Werlte": 9407,
-//     "Gesamt": 325657,
-// };
+const population = {
+    "Samtgemeinde Dörpen": 15672,
+    "Einheitsgemeinde Emsbüren": 9886,
+    "Samtgemeinde Freren": 5113,
+    "Einheitsgemeinde Geeste": 11337,
+    "Stadt Haren (Ems)": 23029,
+    "Stadt Haselünne": 12840,
+    "Samtgemeinde Herzlake": 4097,
+    "Samtgemeinde Lathen": 11122,
+    "Samtgemeinde Lengerich": 22234,
+    "Stadt Lingen (Ems)": 54708,
+    "Stadt Meppen": 34862,
+    "Samtgemeinde Nordhümmling": 12226,
+    "Stadt Papenburg": 35268,
+    "Einheitsgemeinde Rhede (Ems)": 4218,
+    "Einheitsgemeinde Salzbergen": 7590,
+    "Samtgemeinde Sögel": 15923,
+    "Samtgemeinde Spelle": 8442,
+    "Einheitsgemeinde Twist": 9621,
+    "Samtgemeinde Werlte": 9407,
+    "Gesamt": 325657,
+};
 
 function fetchCSVData(url){
     console.log("fetch data start");
-    d3.csv(url, function (data) {
+    return d3.csv(url, function (data) {
         var parseTime = d3.timeParse("%Y-%m-%d")
+        var factor100k = (population[data.kommune] / 100000)
         return {
             datumDate:  parseTime(data['#datum']),
             datumString: data['#datum'],
             kommune: data.kommune,
             faelle: +data.faelle,
+            faelle100k: +data.faelle/factor100k,
             genesen: +data.genesen,
+            genesen: +data.genesen/factor100k,
             verstorben: +data.verstorben,
-            current: +data.current
+            verstorben100k: +data.verstorben/factor100k,
+            current: +data.current,
+            current100k: +data.current/factor100k
         };
     }).then(function (data) {
         generateAllDataGraph(data);
-        console.log(data)
+        csvData = data
+        console.log("fetch data end");
     });
-    console.log("fetch data end");
 }
 
 function generateAllDataGraph(data) {
-    console.log("START generateFetchData");
+    console.log("START generateAllDataGraph");
     var xValues = Array.from(d3.group(data, d => d.datumString).keys())
     var dataSets = Array.from(d3.group(data, d => d.kommune));
 
@@ -58,7 +63,7 @@ function generateAllDataGraph(data) {
         grey: 'rgb(201, 203, 207)',
     };
 
-    var config = {
+    var chart = {
         type: 'line',
         data: {
             labels: xValues,
@@ -99,9 +104,9 @@ function generateAllDataGraph(data) {
 
     var ds = dataSets.forEach(function(d){
         var colorNames = Object.keys(window.chartColors);
-        var col = colorNames[config.data.datasets.length % colorNames.length];
-        //if(d[0]!=='Gesamt')
-        //{
+        var col = colorNames[chart.data.datasets.length % colorNames.length];
+        // if(isKommuneSelected(d[0]))
+        // {
             var newDataset = {
 				label: d[0],
 				backgroundColor: col,
@@ -109,24 +114,26 @@ function generateAllDataGraph(data) {
 				data: d[1].map(d => d.faelle),
                 fill: false,
                 hidden: true
-            };
-            config.data.datasets.push(newDataset);
-        //}
+            }
+            chart.data.datasets.push(newDataset);
+        // }
     })
     
 	var ctx = document.getElementById('canvas').getContext('2d');
-    window.myLine = new Chart(ctx, config);
+    window.myLine = new Chart(ctx, chart);
 
-    console.log("END generateFetchData");
+    console.log("END generateAllDataGraph");
 }
 
 window.addEventListener("load", function () {
-    loadData()
-});
-
-function loadData(){
     console.log("START");
     var csvRawUrl = "https://raw.githubusercontent.com/quatzli/quatzli.github.io/main/corona/zahlen_emsland.csv";
     fetchCSVData(csvRawUrl);
     console.log("END");
+});
+
+function loadData(){
+    console.log("START loadData")
+    generateAllDataGraph(csvData)
+    console.log("END loadData")
 }
